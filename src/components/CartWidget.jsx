bb7fa966e3291ee841b0859c4c98c4d80b688624
Abril Herrada <abrilherrada@gmail.com>
2022-04-18 23:46:50 -0300

    Proyecto final

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,23 +1,33 @@
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { useUserContext } from '../context/UserContext';
 import { useCartContext } from '../context/CartContext';
 import '../styles/css/CartWidget.css';
 
 function CartWidget() {
+    const { user } = useUserContext();
     const { cartList, totalQuantity } = useCartContext();
 
     return (
-        <NavLink to="/cart" className="cartButton">
-            {cartList.length === 0 ? (
-                <FontAwesomeIcon icon={faCartShopping} />
+        <>
+            {user ? (
+                <NavLink to="/cart" className="cartButton">
+                    {cartList.length === 0 ? (
+                        <FontAwesomeIcon icon={faCartShopping} />
+                    ) : (
+                        <>
+                            <FontAwesomeIcon icon={faCartShopping} />
+                            <div className="cartQuantity">
+                                {totalQuantity()}
+                            </div>
+                        </>
+                    )}
+                </NavLink>
             ) : (
-                <>
-                    <FontAwesomeIcon icon={faCartShopping} />
-                    <div className="cartQuantity">{totalQuantity()}</div>
-                </>
+                <></>
             )}
-        </NavLink>
+        </>
     );
 }
 
